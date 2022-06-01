b1bfc0d064f21356c40593204e9e8a6569b8aab0
VikasReddy <vikas.reddy@tyfone.com>
2022-06-01 12:08:39 +0530

    firstproject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todoapp';
+  title = 'TODO WORK STATUS';
+  todoList: { id: string; val: string }[] = [];
+  ongoinglist: { id: string; val: string }[] = [];
+
+  onAdd(todo: string) {
+    todo !== '' ? this.todoList.push({ id: 'one', val: todo }) : null;
+  }
+  
+  movetoOngoing(sd: any) {
+    this.todoList[sd].id = 'two';
+  }
+  moveDone(xd:number) {
+    let value = confirm('are you sure!!  it cant be undone');
+    console.log(value);
+    value === true ? (this.todoList[xd].id = 'three') : null;
+  }
+  delTodo(d:number) {
+    this.todoList.splice(d, 1);
+  }
 }
